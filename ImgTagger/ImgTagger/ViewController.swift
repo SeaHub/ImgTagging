d9acb5d22d6d1e0f3a20d383b93ee35f40559a30
SeaHub <18933932386@163.com>
2017-06-24 15:29:13 +0800

    Initialize everything

---
+++ ImgTagger/ImgTagger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImgTagger
+//
+//  Created by SeaHub on 2017/6/24.
+//  Copyright © 2017年 SeaCluster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
